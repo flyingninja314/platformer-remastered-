a22c1927a4a38d316fc3fd3228c7f54f8da8543a
Daniel Wright <daniel.wright.314@gmail.com>
2015-02-03 13:25:24 -0800

    3 February 2015
    video 11

--- core/src/com/wrightd/platformer/view/GameScreen.java
+++ core/src/com/wrightd/platformer/view/GameScreen.java
@@ -2,6 +2,7 @@ package com.wrightd.platformer.view;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -26,6 +27,9 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0.41f, 0.41f, 0.41f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         camera.update();
         renderer.setView(camera);
         renderer.render();
