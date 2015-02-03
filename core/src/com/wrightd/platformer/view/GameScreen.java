f3f44ac977ea16f1524cff3e70ab0d0da6eda331
Daniel Wright <daniel.wright.314@gmail.com>
2015-02-03 13:09:04 -0800

    3 February 2015
    video 9 (i wrote video 8 twice and video 7 no times)

--- core/src/com/wrightd/platformer/view/GameScreen.java
+++ core/src/com/wrightd/platformer/view/GameScreen.java
@@ -16,6 +16,7 @@ public class GameScreen implements Screen {
         map = new TmxMapLoader().load("map/level01.tmx");
         renderer = new OrthogonalTiledMapRenderer(map, 1/70f);
         camera = new OrthographicCamera(14f, 14f);
+        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
     }
 
     @Override
