e30869db2dbd5d7591f394d7d9ef0b170d0c7b0a
Daniel Wright <daniel.wright.314@gmail.com>
2015-02-05 13:37:41 -0800

    5 February 2015
    video 17

--- core/src/com/wrightd/platformer/model/Player.java
+++ core/src/com/wrightd/platformer/model/Player.java
@@ -2,6 +2,7 @@ package com.wrightd.platformer.model;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -10,9 +11,11 @@ public class Player {
     public Vector2 position;
     public Texture spriteSheet;
     public TextureRegion[] spriteFrames;
+    public Animation animation;
+    private float stateTime;
 
     public Player() {
-        position = new Vector2(2, 0);
+        position = new Vector2(0, 2);
         spriteSheet = new Texture(Gdx.files.internal("img/aliens.png"));
 
         TextureRegion[][] spriteSheetFrames = TextureRegion.split(spriteSheet, 70, 100);
@@ -32,13 +35,20 @@ public class Player {
                 spriteFrames[counter++] = sprite;
             }
         }
+
+        TextureRegion[] animationFrames = new TextureRegion[2];
+        animationFrames[0] = spriteFrames[9];
+        animationFrames[1] = spriteFrames[10];
+        animation = new Animation(.125f, animationFrames);
+
+        stateTime = 0f;
     }
 
     public void draw(Batch spriteBatch) {
-        spriteBatch.draw(spriteSheet[34], position.x, position.y, 70 * (1/70f), 100 * (1/70f));
+        spriteBatch.draw(animation.getKeyFrame(stateTime, true), position.x, position.y, 70 * (1/70f), 100 * (1/70f));
     }
 
     public void update(float deltaTime) {
-
+        stateTime += deltaTime;
     }
 }
