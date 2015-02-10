fdc295eef785c449e4351c38a9c6fed6a0852804
Daniel Wright <daniel.wright.314@gmail.com>
2015-02-10 14:02:50 -0800

    10 February 2015
    video 24

--- core/src/com/wrightd/platformer/model/Player.java
+++ core/src/com/wrightd/platformer/model/Player.java
@@ -4,26 +4,32 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.HashMap;
+
 public class Player {
     public Vector2 position;
-    public Animation animation;
     public Spritesheet spriteSheet;
     public int width;
     public int height;
+    public String currentAnimation;
     private float stateTime;
+    private HashMap<String, Animation> animations;
 
     public Player() {
         position = new Vector2(0, 2);
         width = 70;
         height = 100;
         spriteSheet = new Spritesheet("img/aliens.png", width, height);
-        animation = spriteSheet.createAnimation(9, 10, 0.125f);
-        animation = spriteSheet.flipAnimation(animation, true, false);
+        animations = new HashMap<String, Animation>();
+
+        animations.put("walkRight", spriteSheet.createAnimation(9, 10, 0.125f));
+        animations.put("walkLeft", spriteSheet.flipAnimation(animations.get("walkRight"), true, false));
+        currentAnimation = "walkRight";
         stateTime = 0f;
     }
 
     public void draw(Batch spriteBatch) {
-        spriteBatch.draw(animation.getKeyFrame(stateTime, true), position.x, position.y, width * (1/70f), height * (1/70f));
+        spriteBatch.draw(animations.get(currentAnimation).getKeyFrame(stateTime, true), position.x, position.y, width * (1/70f), height * (1/70f));
     }
 
     public void update(float deltaTime) {
