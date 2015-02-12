a59f24aa132fc0e2ca3ade5627e99c5a399197b1
Daniel Wright <daniel.wright.314@gmail.com>
2015-02-12 13:57:14 -0800

    12 February 2015
    video 26

--- core/src/com/wrightd/platformer/model/Player.java
+++ core/src/com/wrightd/platformer/model/Player.java
@@ -3,6 +3,11 @@ package com.wrightd.platformer.model;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.wrightd.platformer.view.GameScreen;
 
 import java.util.HashMap;
 
@@ -22,20 +27,44 @@ public class Player {
         spriteSheet = new Spritesheet("img/aliens.png", width, height);
         animations = new HashMap<String, Animation>();
 
+        BodyDef bodyDefinition = new BodyDef();
+        bodyDefinition.type = BodyDef.BodyType.DynamicBody;
+        bodyDefinition.position.set(position);
+
+        Body playerBody = GameScreen.gameWorld.createBody(bodyDefinition);
+        playerBody.setUserData(this);
+
+        PolygonShape rectangleShape = new PolygonShape();
+        rectangleShape.setAsBox(width / 2f, height / 2f, new Vector2(width / 2f, height / 2f), 0);
+
+        FixtureDef fixtureDefinition = new FixtureDef();
+        fixtureDefinition.shape = rectangleShape;
+
+        playerBody.createFixture(fixtureDefinition);
+        rectangleShape.dispose();
+
         animations.put("walkRight", spriteSheet.createAnimation(31, 32, 0.125f));
         animations.put("walkLeft", spriteSheet.flipAnimation(animations.get("walkRight"), true, false));
+
         animations.put("jumpRight", spriteSheet.createAnimation(27, 27, 0.125f));
         animations.put("jumpLeft", spriteSheet.flipAnimation(animations.get("jumpRight"), true, false));
+
         animations.put("duckRight", spriteSheet.createAnimation(25, 25, 0.125f));
         animations.put("duckLeft", spriteSheet.flipAnimation(animations.get("jumpRight"), true, false));
+
         animations.put("hurtRight", spriteSheet.createAnimation(26, 26, 0.125f));
         animations.put("hurtLeft", spriteSheet.flipAnimation(animations.get("jumpRight"), true, false));
+
         animations.put("idleRight", spriteSheet.createAnimation(28, 28, 0.125f));
         animations.put("idleLeft", spriteSheet.flipAnimation(animations.get("jumpRight"), true, false));
+
         animations.put("swimRight", spriteSheet.createAnimation(29, 30, 0.125f));
         animations.put("swimLeft", spriteSheet.flipAnimation(animations.get("jumpRight"), true, false));
+
         animations.put("climb", spriteSheet.createAnimation(23, 24, 0.125f));
+
         animations.put("stand", spriteSheet.createAnimation(22, 22, 0.125f));
+
         currentAnimation = "walkRight";
         stateTime = 0f;
     }
