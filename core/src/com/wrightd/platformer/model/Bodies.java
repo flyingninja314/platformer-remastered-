9b4c29a0add0cbf8f7535e5e620e9f68bfd5e01b
Daniel Wright <daniel.wright.314@gmail.com>
2015-02-18 20:24:17 -0800

    18 February 2015
    video 39

---
+++ core/src/com/wrightd/platformer/model/Bodies.java
@@ -0,0 +1,34 @@
+package com.wrightd.platformer.model;
+
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.wrightd.platformer.controller.LevelController;
+
+public class Bodies {
+
+    public static void createBody(MapObject mapObject) {
+        String bodyType = mapObject.getProperties().get("type").toString();
+
+        if(bodyType .equalsIgnoreCase("solid")) {
+            RectangleMapObject rectangleObject = (RectangleMapObject)mapObject;
+            BodyDef bodyDefinition = new BodyDef();
+            bodyDefinition.type = BodyDef.BodyType.StaticBody;
+            bodyDefinition.position.set(rectangleObject.getRectangle().x * LevelController.UNIT_SCALE, rectangleObject.getRectangle().y * LevelController.UNIT_SCALE);
+
+            Body physicsBody = LevelController.gameWorld.createBody(bodyDefinition);
+            PolygonShape rectangleShape = new PolygonShape();
+            rectangleShape.setAsBox(rectangleObject.getRectangle().width * LevelController.UNIT_SCALE / 2f, rectangleObject.getRectangle().height * LevelController.UNIT_SCALE / 2f, new Vector2(rectangleObject.getRectangle().width * LevelController.UNIT_SCALE / 2f, rectangleObject.getRectangle().height * LevelController.UNIT_SCALE / 2f), 0f);
+
+            FixtureDef fixtureDefinition = new FixtureDef();
+            fixtureDefinition.shape = rectangleShape;
+
+            physicsBody.createFixture(fixtureDefinition);
+            rectangleShape.dispose();
+        }
+    }
+}
