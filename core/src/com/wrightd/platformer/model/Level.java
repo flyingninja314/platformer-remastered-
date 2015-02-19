419daa4ef481e1009af704604ec582153da36284
Daniel Wright <daniel.wright.314@gmail.com>
2015-02-18 19:45:54 -0800

    18 February 2015
    video 38
    
    we didnt use getMapLayer and getLayerObjects so
    yeah
    i expect that in the next video

--- core/src/com/wrightd/platformer/model/Level.java
+++ core/src/com/wrightd/platformer/model/Level.java
@@ -1,5 +1,7 @@
 package com.wrightd.platformer.model;
 
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 
@@ -8,5 +10,13 @@ public class Level {
 
     public Level(String mapPath) {
         map = new TmxMapLoader().load(mapPath);
+    }
+
+    public MapLayer getMapLayer(String layerName) {
+        return map.getLayers().get(layerName);
+    }
+
+    public MapObjects getLayerObjects(MapLayer mapLayer) {
+        return mapLayer.getObjects();
     }
 }
