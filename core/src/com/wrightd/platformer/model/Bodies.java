9840ff53274d8c43121ff6dd6bfdd6b71c98a311
Daniel Wright <daniel.wright.314@gmail.com>
2015-02-19 18:38:27 -0800

    19 February 2015
    video 43-which-is-apparently-now-the-number-that-follows-53

--- core/src/com/wrightd/platformer/model/Bodies.java
+++ core/src/com/wrightd/platformer/model/Bodies.java
@@ -2,10 +2,12 @@ package com.wrightd.platformer.model;
 
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.PolygonMapObject;
+import com.badlogic.gdx.maps.objects.PolylineMapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.ChainShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.wrightd.platformer.controller.LevelController;
@@ -15,21 +17,20 @@ public class Bodies {
     public static void createBody(MapObject mapObject) {
         String bodyType = mapObject.getProperties().get("type").toString();
 
-        if(bodyType .equalsIgnoreCase("solid")) {
-            RectangleMapObject rectangleObject = (RectangleMapObject)mapObject;
+        if(bodyType .equalsIgnoreCase("ground")) {
+            PolylineMapObject polylineMapObject = (PolylineMapObject)mapObject;
             BodyDef bodyDefinition = new BodyDef();
             bodyDefinition.type = BodyDef.BodyType.StaticBody;
-            bodyDefinition.position.set(rectangleObject.getRectangle().x * LevelController.UNIT_SCALE, rectangleObject.getRectangle().y * LevelController.UNIT_SCALE);
+            bodyDefinition.position.set(polylineMapObject.getPolyline().getX() * LevelController.UNIT_SCALE, polylineMapObject.getPolyline().getY() * LevelController.UNIT_SCALE);
 
             Body physicsBody = LevelController.gameWorld.createBody(bodyDefinition);
-            PolygonShape rectangleShape = new PolygonShape();
-            rectangleShape.setAsBox(rectangleObject.getRectangle().width * LevelController.UNIT_SCALE / 2f, rectangleObject.getRectangle().height * LevelController.UNIT_SCALE / 2f, new Vector2(rectangleObject.getRectangle().width * LevelController.UNIT_SCALE / 2f, rectangleObject.getRectangle().height * LevelController.UNIT_SCALE / 2f), 0f);
+            ChainShape chainShape = new ChainShape();
 
             FixtureDef fixtureDefinition = new FixtureDef();
-            fixtureDefinition.shape = rectangleShape;
+            fixtureDefinition.shape = chainShape;
 
             physicsBody.createFixture(fixtureDefinition);
-            rectangleShape.dispose();
+            chainShape.dispose();
         }
         else if(bodyType .equalsIgnoreCase("slope")) {
             PolygonMapObject polygonObject = (PolygonMapObject)mapObject;
